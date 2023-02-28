59e56b1756d99d563330d44b877118c8bc2e633d
Ylian Fatmi <yli.fatmi@gmail.com>
2023-02-28 10:36:11 +0100

    ajout bouton AddTask

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.addButton)
+
+        btn.setOnClickListener() {
+
+            val intent = Intent(this@MainActivity, AddTask::class.java)
+            startActivity(intent)
+
+        }
     }
 }
